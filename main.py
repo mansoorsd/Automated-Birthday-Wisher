335ffc661d93f602e0dcdb90ade6dd7dc464fce0
Mansoor Basha Syed <83864755+mansoorsd@users.noreply.github.com>
2021-06-06 15:47:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import datetime as dt
+import pandas as pd
+import smtplib
+import random
+
+# ----------------------------------- BIRTHDAY DATE CHECK ----------------------------- #
+
+# Configuring the required data
+letter_list = ["letter_1.txt", "letter_2.txt", "letter_3.txt"]
+data = pd.read_csv("birthdays.csv")
+my_email = "testudemy0511@gmail.com"
+password = "Mansoor@6677"
+PLACE_HOLDER = "[NAME]"
+
+# Getting hold of today's date
+today = dt.datetime.now()
+month = today.month
+day = today.day
+
+# Importing Data from CSV
+for index, row in data.iterrows():
+    person_name = row['name']
+    person_email = row["email"]
+
+    # Comparing date of birth with today's date
+    if row['month'] == month and row['day'] == day:
+        letter_number = random.choice(letter_list)
+        with open(f"letter_templates/{letter_number}") as message:
+            email_message = message.read()
+
+        # Editing Mail content
+        email_message = email_message.replace(PLACE_HOLDER, person_name)
+
+        # Initiating the mail
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs=row["email"],
+                msg=f"Subject:Surprise \n\n{email_message}")
+
+
